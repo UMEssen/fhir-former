b42ab5e08f1097fb6935f820be85c172e5313196
merlinengelke <merlin.engelke@uk-essen.de>
2023-08-21 10:31:19 +0200

    updated streamlit app

--- app/app_icd.py
+++ app/app_icd.py
@@ -14,26 +14,44 @@ from ml.ds_main_diag_llm import PatientEncounterDataset as PatientEncounterDatas
 import icd10
 from annotated_text import annotated_text
 
+# Config
+st.set_page_config(
+    page_title="SHIPFormer",
+    page_icon=":male_mage:",
+    layout="wide",
+    initial_sidebar_state="auto",
+)
+
+
+@st.cache_data
+def load_data(file_path):
+    with open(file_path, "rb") as f:
+        return json.load(f)
+
+@st.cache_resource
+def load_model(model_path):
+    return AutoModelForSequenceClassification.from_pretrained(model_path)
+
+@st.cache_data
+def load_tokenizer():
+    return AutoTokenizer.from_pretrained("LennartKeller/longformer-gottbert-base-8192-aw512")
+
+@st.cache_data
+def get_patient_data(df_filtered, patient_id):
+    return df_filtered[df_filtered["patient_id"] == patient_id]
+
+@st.cache_data
+def get_filtered_data(patient_data, selected_date):
+    return patient_data[patient_data['text'].str.contains(f"sample_date\t{selected_date}")]
 
 # Load the data and model for icd prediction
-with open("/local/work/merengelke/ship_former/ds_icd_test_samples.json", "rb") as f:
-    data = json.load(f)
+data = load_data("/local/work/merengelke/ship_former/ds_icd_test_samples.json")
+model = load_model("/local/work/merengelke/ship_former/models/models")
+tokenizer = load_tokenizer()
 
-
-
-model_name_or_path = "/local/work/merengelke/ship_former/models/models"
-model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path)
-tokenizer = AutoTokenizer.from_pretrained(
-    "LennartKeller/longformer-gottbert-base-8192-aw512"
-)
-
-ds = PatientEncounterDatasetICD(
-    "/local/work/merengelke/ship_former/ds_icd_test_samples.json", tokenizer
-)
 ds_train = PatientEncounterDatasetICD(
     "/local/work/merengelke/ship_former/ds_icd_train_samples.json", tokenizer
 )
-pats = pd.read_feather("/local/work/merengelke/ship_former/data_raw/patient.ftr")
 
 labels = [label for label in ds_train[0]["label_codes"]]
 id2label = {idx: label for idx, label in enumerate(labels)}
@@ -42,9 +60,11 @@ label2id = {label: idx for idx, label in enumerate(labels)}
 if "prediction_made" not in st.session_state:
     st.session_state.prediction_made = False
 
-if "annotated_results" not in st.session_state:
-    st.session_state.annotated_results = None
+# if "annotated_results" not in st.session_state:
+#     st.session_state.annotated_results = None
 
+annotated_results = None
+predicted_labels_str = None
 
 # Load the data and model for drg prediction
 with open(
@@ -54,8 +74,7 @@ with open(
 
 
 
-model_name_or_path_drg = "/local/work/merengelke/ship_former/models/models"
-model_drg = AutoModelForSequenceClassification.from_pretrained(model_name_or_path_drg)
+model_drg_path = "/local/work/merengelke/ship_former/ds_main_diag/models/whaleloops_KEPTlongformer-PMM3_2023-08-17_08-57/models"
 if "combined_text" not in st.session_state:
     st.session_state.combined_text = None
 
@@ -123,9 +142,9 @@ def compare_labels(predicted_labels, true_labels):
 
 # Helper classes
 class ICDClassifier:
-    def __init__(self, model_path, id_to_label):
+    def __init__(self, model_path, tokenizer_name: str, id_to_label):
         self.model = AutoModelForSequenceClassification.from_pretrained(model_path)
-        self.tokenizer = AutoTokenizer.from_pretrained("allenai/biomed_roberta_base")
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
         self.id_to_label = id_to_label  # Ensure you use this instead of id_to_label
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
@@ -146,13 +165,6 @@ class ICDClassifier:
         # return predicted_class  # Use this if you want to return the predicted class index
 
 
-# Config
-st.set_page_config(
-    page_title="DRG-Main Diagnosis Prediction",
-    page_icon=":male_mage:",
-    layout="wide",
-    initial_sidebar_state="auto",
-)
 
 # Sidebar
 pages = {"drg": "Main Diagnosis Prediction"}
@@ -160,9 +172,9 @@ page_order = ["drg"]
 
 
 with st.sidebar:
-    st.image(render_png(Path("app/.streamlit/SHIPFormer.png")))
+    st.image(render_png(Path("app/.streamlit/FHIRFormer.png")))
     st.write(
-        "SHIP-FORMER, developed by SHIP-AI at the Institute"
+        "SHIPFormer, developed by SHIP-AI at the Institute"
         " for Artificial Intelligence in Medicine, is an advanced"
         " tool in the Smart Hospital Information Platform that"
         " leverages robust medical evaluations and recommendations"
@@ -195,37 +207,41 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# st.markdown('<h2 class="title-centered">SHIP-FORMER</h2>', unsafe_allow_html=True)
+# sentence = "Foreseeing Outcomes through Robust Medical Evaluations and Recommendations."
+# formatted_sentence = " ".join(
+#     [
+#         f'<span style="font-size: larger; font-weight: bold;">{word[0]}</span>{word[1:]}'
+#         if word.lower() != "for"
+#         else word
+#         for word in sentence.split()
+#     ]
+# )
 
-sentence = "Smart Hospital Information Platform - Foreseeing Outcomes through Robust Medical Evaluations and Recommendations."
-formatted_sentence = " ".join(
-    [
-        f'<span style="font-size: larger; font-weight: bold;">{word[0]}</span>{word[1:]}'
-        if word.lower() != "for"
-        else word
-        for word in sentence.split()
-    ]
-)
-
-st.markdown(
-    f'<h4 class="title-centered">{formatted_sentence}</h4>', unsafe_allow_html=True
-)
+# st.markdown(
+#     f'<h4 class="title-centered">{formatted_sentence}</h4>', unsafe_allow_html=True
+# )
 
 # Create two columns: left_column and right_column
 left_column, right_column = st.columns(2)
 
-# samples only contain patients with more than 4 samples
-x = pd.DataFrame([x['label'] for x in data])
-df = pd.DataFrame(data)
-df['count'] = df.groupby(['patient_id', 'encounter_id'])['patient_id'].transform('count')
-df_filtered = df[df['count'] > 4]
-pats = df_filtered["patient_id"].unique()
-# take 10 random patients
-pats = random.sample(list(pats), 10)
-
-# If random_samples doesn't already exist in session state, create it
 if "input_text" not in st.session_state:
-    st.session_state.input_text = df_filtered[df_filtered["patient_id"] == pats[0]]["text"]
+    # samples only contain patients with more than 4 samples
+    x = pd.DataFrame([x['label'] for x in data])
+    df = pd.DataFrame(data)
+    df['count'] = df.groupby(['patient_id', 'encounter_id'])['patient_id'].transform('count')
+    df_filtered = df[df['count'] > 4]
+    pats = df_filtered["patient_id"].unique()
+    # take 10 random patients
+    st.session_state.pats = random.sample(set(list(pats)), 10)
+    st.session_state.pats.pop(-1)
+    st.session_state.pats = ['6a910c295c3cd7eaa8933c70fd17745959c2e1ee6bb9b4f98aa278f9d0fe1536'] + st.session_state.pats
+    # Filter the df_filtered DataFrame based on the random patient IDs using .isin()
+    filtered_data = df_filtered[df_filtered['patient_id'].isin(st.session_state.pats)]
+    # Store the filtered data in session state
+    st.session_state.pats_data = filtered_data
+
+    # If random_samples doesn't already exist in session state, create it
+    st.session_state.input_text = st.session_state.pats_data[st.session_state.pats_data["patient_id"] == pats[0]]["text"]
 
 if "patient_data_filtered" not in st.session_state:
     st.session_state.patient_data_filtered = None
@@ -236,14 +252,17 @@ if "selected_date" not in st.session_state:
 if "selected_patient" not in st.session_state:
     st.session_state.selected_patient = None
 
+# good for demo: 070f17e8901093eb169aef0c8ab16f545b96bf43b636f8a100196a4d9b11f9a9
+# 6a910c295c3cd7eaa8933c70fd17745959c2e1ee6bb9b4f98aa278f9d0fe1536
+
 # Populate the left column with patient selection and information
 with left_column:
     # Allow user to select a patient
-    patient_id = st.selectbox("**Choose a patient**", pats)
+    patient_id = st.selectbox("**Choose a patient**", st.session_state.pats)
     st.session_state.selected_patient = patient_id
     if patient_id:
         # get all ds samples for this patient for ds[]
-        patient_data = df_filtered[df_filtered["patient_id"] == patient_id]
+        patient_data = get_patient_data(st.session_state.pats_data, patient_id)
 
         # extract date by pat data
         dates = [pat.split('sample_date\t')[1].split('\n')[0] for pat in patient_data['text']]
@@ -251,19 +270,17 @@ with left_column:
 
         # add slider to select date
         st.session_state.selected_date = st.select_slider('Select Date', options=dates)  # create a slider to select date
-        print(st.session_state.selected_date)
 
         # Filter the data by selected date
-        st.session_state.patient_data_filtered = patient_data[
-            patient_data['text'].str.contains(f"sample_date\t{st.session_state.selected_date}")]
+        st.session_state.patient_data_filtered = get_filtered_data(patient_data, st.session_state.selected_date)
 
         # Get the selected patient's data
         st.session_state.input_text = st.session_state.patient_data_filtered["text"].iloc[0]
-        formatted_text = st.session_state.patient_data_filtered["text"].iloc[0].replace("\n", "  \n")
-        formatted_text = formatted_text.replace("\t", "  \t")
+        st.session_state.input_text = st.session_state.patient_data_filtered["text"].iloc[0].replace("\n", "  \n")
+        st.session_state.input_text = st.session_state.input_text.replace("\t", "  \t")
 
         st.text_area(
-            label="**Patient Information**", value=f"{formatted_text}", height=600
+            label="**Patient Information**", value=f"{st.session_state.input_text}", height=600
         )
 
 # Populate the right column with the prediction result
@@ -293,7 +310,7 @@ with right_column:
             predicted_labels, st.session_state.patient_data_filtered["label"].values[0]
         )
 
-        st.session_state.annotated_results = annotated_results
+        # st.session_state.annotated_results = annotated_results
 
         # Combine patient_data['text'] and predicted_labels into a single string
         for index, label in enumerate(predicted_labels):
@@ -301,34 +318,38 @@ with right_column:
 
         predicted_labels_str = "\n ".join(map(str, predicted_labels))
         st.session_state.combined_text = (
-            patient_data["text"]
+            st.session_state.input_text
             + "\n\n"
             + "Predicted labels: \n"
             + predicted_labels_str
         )
         st.session_state.prediction_made = True
 
-    if st.session_state.annotated_results:
+    if annotated_results:
         st.write("**Predicted Labels**")
-        annotated_text(*st.session_state.annotated_results)
+        annotated_text(*annotated_results)
 
-    # # Check if combined_text is not None before proceeding
-    # if st.session_state.prediction_made:
-    #     if st.button("Estimate Main Diagnosis (DRG)"):
-    #         if st.session_state.combined_text is None:
-    #             st.warning(
-    #                 "Please first get the ICD-10-CM Prediction before calculating the final DRG code."
-    #             )
-    #
-    #         id_to_label = {idx: item["label"] for idx, item in enumerate(drg_samples)}
-    #         classifier = ICDClassifier(model_name_or_path_drg, id_to_label)
-    #         txt = (
-    #             st.session_state.combined_text
-    #             if st.session_state.combined_text
-    #             else patient_data["text"]
-    #         )
-    #         predicted_icd = classifier.predict(txt)
-    #         if predicted_icd:
-    #             icd = get_icd_description(predicted_icd)
-    #         st.write(f"**Predicted Main Diagnosis**")
-    #         st.write(icd)
+    with left_column:
+        if predicted_labels_str:
+            st.text_area(label="**Prediction**", value=predicted_labels_str, height=200)
+
+    # Check if combined_text is not None before proceeding
+    if st.session_state.prediction_made:
+        if st.button("Estimate Main Diagnosis (DRG)"):
+            if st.session_state.combined_text is None:
+                st.warning(
+                    "Please first get the ICD-10-CM Prediction before calculating the final DRG code."
+                )
+
+            id_to_label = {idx: item["label"] for idx, item in enumerate(drg_samples)}
+            classifier = ICDClassifier(model_drg_path, "whaleloops/KEPTlongformer-PMM3", id_to_label)
+            st.session_state.input_text = st.session_state.combined_text
+
+
+            # st.session_state.input_text = st.session_state.input_text.replace("\t", "  \t")
+            predicted_icd = classifier.predict(st.session_state.combined_text)
+
+            if predicted_icd:
+                icd = get_icd_description(predicted_icd)
+            exp = st.expander(label=f"**Predicted Main Diagnosis**", expanded=True)
+            exp.write(icd)
