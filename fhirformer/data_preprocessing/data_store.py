8fded84168d7b48631568b574a871b51c188e089
Giulia Baldini <giulia.baldini.11@gmail.com>
2023-09-30 15:54:38 +0200

    Change make timezone aware to have an errors parameter for timestamps that don't exist

--- fhirformer/data_preprocessing/data_store.py
+++ fhirformer/data_preprocessing/data_store.py
@@ -1,12 +1,17 @@
+import traceback
 from dataclasses import dataclass
 import pandas as pd
 from typing import Dict
+from pandas._libs.tslibs.np_datetime import OutOfBoundsDatetime
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-def make_timezone_aware(value, default_timezone="UTC"):
+def make_timezone_aware(value, default_timezone="UTC", errors="coerce"):
     if isinstance(value, pd.Series):
         # Convert to datetime Series and ensure it's timezone-aware
-        datetime_series = pd.to_datetime(value, utc=True).dt.tz_convert(
+        datetime_series = pd.to_datetime(value, utc=True, errors=errors).dt.tz_convert(
             default_timezone
         )
         return datetime_series
@@ -16,7 +21,7 @@ def make_timezone_aware(value, default_timezone="UTC"):
             return value.tz_localize(default_timezone)
         return value
     elif isinstance(value, str):
-        timestamp = pd.to_datetime(value)
+        timestamp = pd.to_datetime(value, errors=errors)
         if not timestamp.tz:
             return timestamp.tz_localize(default_timezone)
         return timestamp
@@ -36,18 +41,29 @@ def select_resources(
     if len(resource_df) == 0:
         return resource_df
 
-    condition = resource_df.patient_id == patient_id
-    resource_tz_column = make_timezone_aware(resource_df[column])
+    try:
+        resource_for_patient = resource_df.patient_id == patient_id
+        resource_tz_column = make_timezone_aware(resource_df[column])
 
-    if start_filter_date:
-        start_filter_date = make_timezone_aware(start_filter_date)
-        condition &= resource_tz_column >= start_filter_date
+        if start_filter_date:
+            start_filter_date = make_timezone_aware(start_filter_date)
+            resource_for_patient &= resource_tz_column >= start_filter_date
 
-    if end_filter_date:
-        end_filter_date = make_timezone_aware(end_filter_date)
-        condition &= resource_tz_column <= end_filter_date
+        if end_filter_date:
+            end_filter_date = make_timezone_aware(end_filter_date)
+            resource_for_patient &= resource_tz_column <= end_filter_date
+        return resource_df.loc[resource_for_patient]
 
-    return resource_df.loc[condition]
+    except OutOfBoundsDatetime:
+        traceback.print_exc()
+        logger.error(
+            f"patient_id: {patient_id}, "
+            f"column: {column}, "
+            f"start_filter_date: {start_filter_date}, "
+            f"end_filter_date: {start_filter_date}, "
+            f"resource_df: \n{resource_df}"
+        )
+        raise OutOfBoundsDatetime
 
 
 @dataclass
