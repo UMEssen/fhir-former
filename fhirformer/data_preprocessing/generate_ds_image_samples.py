11297cecf1295d53ab2d69fd9e2837e572f79d53
Giulia Baldini <giulia.baldini.11@gmail.com>
2023-11-03 09:59:32 +0100

    add secondary function for sliding window and make start of sample for normal version one day later

--- fhirformer/data_preprocessing/generate_ds_image_samples.py
+++ fhirformer/data_preprocessing/generate_ds_image_samples.py
@@ -1,6 +1,7 @@
 import logging
 from multiprocessing import Pool
 
+import pandas as pd
 from tqdm import tqdm
 
 from fhirformer.data_preprocessing.encounter_dataset_builder import (
@@ -45,6 +46,81 @@ class ImageDatasetBuilder(EncounterDatasetBuilder):
                 break
         return results
 
+    def process_patient_sliding_window(self, patient_id: str):
+        """
+        - Patient needs at least one imaging study and one encounter
+        """
+        pat_data = datastore.filter_patient(patient_id=patient_id)
+
+        if (
+            len(pat_data.patient_df) == 0
+            or len(pat_data.resources["encounter"]) == 0
+            or len(pat_data.resources["imaging_study"]) == 0
+        ):
+            return []
+
+        sample_list = []
+        for enc in pat_data.resources["encounter"].itertuples(index=False):
+            duration = (enc.end - enc.start).days
+            if duration <= 2:
+                continue
+            patient_metadata_str = (
+                f"Patient metadata:"
+                f"\n{self.pat_df_to_string(pat_data.patient_df, enc.start)}\n\n"
+            )
+            previous_history = None
+            # Generate multiple samples from each encounter
+            for date in pd.date_range(
+                enc.start + pd.Timedelta(days=1), enc.end - pd.Timedelta(days=1)
+            ):
+                # Get imaging study for encounter duration
+                imaging_study_during_enc = pat_data.filter_patient(
+                    patient_id=patient_id,
+                    start_filter_date=date,
+                    end_filter_date=enc.end,
+                    target_resource="imaging_study",
+                    start_inclusive=True,
+                    end_inclusive=True,
+                )
+                # Label explanation can be found here: http://dicomlookup.com/modalities.asp
+                labels = (
+                    imaging_study_during_enc.resources["imaging_study"][
+                        "modality_code"
+                    ].unique()
+                ).tolist()
+
+                # # Currently skip if no labels
+                # if len(labels) == 0:
+                #     continue
+
+                resources_before_date = pat_data.filter_patient(
+                    patient_id=patient_id,
+                    end_filter_date=date,
+                    end_inclusive=False,
+                ).resources
+                pat_hist = self.pat_history_to_string(resources_before_date)
+
+                if not pat_hist or pat_hist == previous_history:
+                    continue
+                previous_history = pat_hist
+
+                text = (
+                    f"{patient_metadata_str}"
+                    f"Encounter:\n{self.enc_to_string(enc)}\n\n"
+                    f"Patient history:\n{pat_hist}"
+                )
+                sample_list.append(
+                    {
+                        "patient_id": str(patient_id),
+                        "encounter_id": enc.encounter_id,
+                        "encounter_start_date": str(enc.start),
+                        "sample_date": str(date),
+                        "text": text,
+                        "labels": labels,
+                    }
+                )
+        return sample_list
+
     def process_patient(self, patient_id: str):
         """
         - Patient needs at least one imaging study and one encounter
@@ -57,10 +133,11 @@ class ImageDatasetBuilder(EncounterDatasetBuilder):
         sample_list = []
         for enc in pat_data.resources["encounter"].itertuples(index=False):
             patient_metadata_str = f"Patient metadata:\n{self.pat_df_to_string(pat_data.patient_df, enc.start)}\n\n"
+            sample_start = enc.start + pd.Timedelta(days=1)
             # Get data before the beginning of this particular encounter
             resources_before_enc = pat_data.filter_patient(
                 patient_id=patient_id,
-                end_filter_date=enc.start,
+                end_filter_date=sample_start,
                 end_inclusive=False,
             ).resources
             pat_hist = self.pat_history_to_string(resources_before_enc)
@@ -71,7 +148,7 @@ class ImageDatasetBuilder(EncounterDatasetBuilder):
             # Get imaging study for encounter duration
             imaging_study_during_enc = pat_data.filter_patient(
                 patient_id=patient_id,
-                start_filter_date=enc.start,
+                start_filter_date=sample_start,
                 end_filter_date=enc.end,
                 target_resource="imaging_study",
                 start_inclusive=True,
@@ -95,7 +172,8 @@ class ImageDatasetBuilder(EncounterDatasetBuilder):
                 {
                     "patient_id": str(patient_id),
                     "encounter_id": enc.encounter_id,
-                    "encounter_start_date": str(enc.start),
+                    "encounter_start": str(enc.start),
+                    "sample_start": str(sample_start),
                     "text": text,
                     "labels": labels,
                 }
